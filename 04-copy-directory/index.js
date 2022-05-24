95325a3762f911c4f6e68f87a522aaea4fa18705
Novikov Stanislav <novikov.stan@mail.ru>
2022-05-24 10:55:16 +0300

    fix: add err callbacks

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,18 +4,26 @@ const srcDir = path.join(__dirname, "/files");
 const outputDir = path.join(__dirname, "/files-copy");
 
 (async function () {
-  await rm(outputDir, { recursive: true, force: true });
-  await mkdir(outputDir);
+  await rm(outputDir, { recursive: true, force: true }, (err) => {
+    if (err) throw err;
+  });
+  await mkdir(outputDir, (err) => {
+    if (err) throw err;
+  });
   copyDir(srcDir, outputDir);
 })();
 
 async function copyDir(src, output) {
-  const files = await readdir(src, { withFileTypes: true });
+  const files = await readdir(src, { withFileTypes: true }, (err) => {
+    if (err) throw err;
+  });
   files.forEach(async function (file) {
     if (file.isFile()) {
       copyFile(`${src}\\${file.name}`, `${output}\\${file.name}`);
     } else if (file.isDirectory()) {
-      await mkdir(`${output}\\${file.name}`);
+      await mkdir(`${output}\\${file.name}`, (err) => {
+        if (err) throw err;
+      });
       await copyDir(`${src}\\${file.name}`, `${output}\\${elem.name}`);
     }
   });
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,16 +5,30 @@ const bundle = path.join(__dirname, "project-dist", "bundle.css");
 const folder = path.join(__dirname, "styles");
 
 (async function () {
-  let files = await fsPromises.readdir(folder, {
-    withFileTypes: true,
-  });
+  let files = await fsPromises.readdir(
+    folder,
+    {
+      withFileTypes: true,
+    },
+    (err) => {
+      if (err) throw err;
+    }
+  );
 
   let styles = [];
   for (let file of files) {
-    let ext = path.extname(path.join(folder, file.name));
+    let ext = path.extname(path.join(folder, file.name), (err) => {
+      if (err) throw err;
+    });
 
     if (ext == ".css") {
-      let css = await fsPromises.readFile(path.join(folder, file.name), "utf8");
+      let css = await fsPromises.readFile(
+        path.join(folder, file.name),
+        "utf8",
+        (err) => {
+          if (err) throw err;
+        }
+      );
       styles.push(`${css}\n`);
     }
   }
